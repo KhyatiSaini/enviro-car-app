7bfac0ba1a432a1f3fe269dfb2ea6c6dce4e6a66
khyatisaini <sainikhyati2001@gmail.com>
2021-04-26 01:04:25 +0530

    adding home screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:envirocar/authentication/sign_in.dart';
 import 'package:envirocar/providers/user_provider.dart';
 import 'package:envirocar/routes/routes.dart';
-import 'package:envirocar/screens/track_details.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import './authentication/sign_up.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,7 +22,7 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.blue,
             unselectedWidgetColor: Colors.grey[200]
         ),
-        home: TrackDetails(),
+        home: HomeScreen(),
         routes: routes,
       ),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,161 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.speed, color: Colors.grey),
+            title: Text('Dashboard', style: TextStyle(color: Colors.lightBlue.shade900)),
+            backgroundColor: Colors.lightBlue.shade800,
+            activeIcon: Icon(Icons.speed, color: Colors.lightBlue.shade800),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_road_outlined, color: Colors.grey),
+              title: Text('Tracks', style: TextStyle(color: Colors.lightBlue.shade900)),
+              backgroundColor: Colors.lightBlue.shade800
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.more_horiz, color: Colors.grey),
+              title: Text('Others', style: TextStyle(color: Colors.lightBlue.shade900)),
+              backgroundColor: Colors.lightBlue.shade800
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: null,
+          margin: null,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.lightBlue.shade800,
+                ),
+              ),
+              Expanded(
+                flex: 10,
+                child: Container(
+                  child: Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.fromLTRB(60, 30, 60, 20),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Colors.lightBlue.shade900,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(
+                          'Recording settings',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  child: FloatingActionButton(
+                                    child: Icon(Icons.bluetooth_rounded),
+                                    backgroundColor: Colors.lightBlue.shade900,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(top: 10),
+                                  child: Text(
+                                    'Bluetooth',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  child: FloatingActionButton(
+                                    child: Icon(Icons.phone_android_outlined),
+                                    backgroundColor: Colors.lightBlue.shade900,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(top: 10),
+                                  child: Text(
+                                    'OBD',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  child: FloatingActionButton(
+                                    child: Icon(Icons.my_location),
+                                    backgroundColor: Colors.lightBlue.shade900,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(top: 10),
+                                  child: Text(
+                                    'GPS',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  child: FloatingActionButton(
+                                    child: Icon(Icons.directions_car),
+                                    backgroundColor: Colors.lightBlue.shade900,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(top: 10),
+                                  child: Text(
+                                    'Car',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
