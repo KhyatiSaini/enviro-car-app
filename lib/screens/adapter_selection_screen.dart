3defa8e646797f46cc018513b1eea37c21869756
khyatisaini <sainikhyati2001@gmail.com>
2021-05-01 13:12:22 +0530

    home screen ui

---
+++ lib/screens/adapter_selection_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdapterSelectionScreen extends StatefulWidget {
+  @override
+  _AdapterSelectionScreenState createState() => _AdapterSelectionScreenState();
+}
+
+class _AdapterSelectionScreenState extends State<AdapterSelectionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
